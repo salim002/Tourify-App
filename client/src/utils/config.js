b60ffae8c784631f32df328969944e80910835e7
MD SALIM <mdsalim.iiitr@gmail.com>
2024-01-31 21:25:46 +0530

    modified the BASE_URL with proper running deployed server

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,3 +1,3 @@
 
 // export const BASE_URL='http://localhost:4000/api/v1'
-export const BASE_URL='https://tourify-mdsalim.onrender.com'
+export const BASE_URL='https://mdsalim-tourify-backend.onrender.com/api/v1'
